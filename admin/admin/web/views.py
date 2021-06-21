1996d2eea6f834e10b8959a3be44bd544debf1fb
mfcorrea <marcelofcorrea@gmail.com>
2021-06-20 21:03:39 -0300

    Implementação da front e backend
    Implementação do frontend em react e backend em django mais parte administrativa em  django + bootstrap

---
+++ admin/admin/web/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render, redirect
+from .forms import diarista_form
+
+# Create your views here.
+from .models import Diarista
+
+
+def cadastrar_diarista(request):
+    if request.method == "POST":
+        form_diarista = diarista_form.DiaristaForm(request.POST, request.FILES)
+        if form_diarista.is_valid():
+            form_diarista.save()
+            return redirect('listar_diaristas')
+    else:
+        form_diarista = diarista_form.DiaristaForm()
+    return render(request, 'form_diarista.html', {'form_diarista': form_diarista})
+
+
+def listar_diaristas(request):
+    diaristas = Diarista.objects.all()
+    return render(request, 'listar_diaristas.html', {'diaristas': diaristas})
+
+
+def editar_diarista(request, diarista_id):
+    diarista = Diarista.objects.get(id=diarista_id)
+    if request.method == "POST":
+        form_diarista = diarista_form.DiaristaForm(request.POST or None, request.FILES, instance=diarista)
+        if form_diarista.is_valid():
+            form_diarista.save()
+            return redirect('listar_diaristas')
+    else:
+        form_diarista = diarista_form.DiaristaForm(instance=diarista)
+    return render(request, 'form_diarista.html', {'form_diarista': form_diarista})
+
+
+def remover_diarista(request, diarista_id):
+    diarista = Diarista.objects.get(id=diarista_id)
+    diarista.delete()
+    return redirect('listar_diaristas')
